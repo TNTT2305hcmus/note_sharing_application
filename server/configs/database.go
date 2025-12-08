8c4e4a92c5ec8c468005c7f4f77b384574921091
TRI THANH <trannguyentrithanh38@gmail.com>
2025-12-08 10:12:44 +0700

    correct .env + get environment var

--- server/configs/database.go
+++ server/configs/database.go
@@ -2,8 +2,10 @@ package configs
 
 import (
 	"context"
+	"fmt"
 	"log"
 	"os"
+	"time"
 
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
@@ -12,20 +14,36 @@ import (
 var DB *mongo.Database
 
 func ConnectDB() {
-	client, err := mongo.Connect(context.TODO(), options.Client().ApplyURI(os.Getenv("CONNECTION_STRING")))
+	// Lấy biến môi trường trong .env
+	mongoURI := os.Getenv("MONGO_URI")
+	dbName := os.Getenv("DB_NAME")
+
+	if mongoURI == "" {
+		log.Fatal("Chưa cấu hình MONGO_URI trong file .env")
+	}
+
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	// Kết nối đến db
+	client, err := mongo.Connect(ctx, options.Client().ApplyURI(mongoURI))
 	if err != nil {
-		log.Fatal(err)
+		log.Fatal("Lỗi khởi tạo client Mongo:", err)
 	}
 
-	if err := client.Ping(context.TODO(), nil); err != nil {
-		log.Fatal("Error pinging MongoDB: ", err)
+	// ping check
+	if err := client.Ping(ctx, nil); err != nil {
+		log.Fatal("Không thể ping tới MongoDB:", err)
 	}
-	log.Println("Connected to MongoDB successfully")
 
-	// Giả sử tên DB là NoteApp
-	DB = client.Database(os.Getenv("DB_NAME"))
+	fmt.Println("Connected to MongoDB successfully")
+
+	DB = client.Database(dbName)
 }
 
 func GetCollection(name string) *mongo.Collection {
+	if DB == nil {
+		log.Fatal("Database chưa được khởi tạo.")
+	}
 	return DB.Collection(name)
 }
