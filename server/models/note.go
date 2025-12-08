a88e36f42340da346b38f28bc7fe8dfd703e897e
TRI THANH <trannguyentrithanh38@gmail.com>
2025-12-08 10:13:07 +0700

    check

--- server/models/note.go
+++ server/models/note.go
@@ -7,10 +7,10 @@ import (
 type Note struct {
 	ID              primitive.ObjectID `bson:"_id,omitempty" json:"note_id"` // omitempty = nếu trường rỗng thì tự sinh ID
 	Title           string             `bson:"title" json:"title"`
-	CipherText      string             `bson:"cipher_text" json:"cipher_text"`             // Nội dung ghi chú đã mã hóa
-	EncryptedAesKey string             `bson:"encrypted_aes_key" json:"encrypted_aes_key"` // Key giải mã (đã bị bọc)
-	OwnerID         string             `bson:"owner_id" json:"owner_id"`                   // ID của người tạo (dạng string)
-	ReceiverID      string             `bson:"receiver_id" json:"receiver_id"`             // ID của người nhận
+	CipherText      string             `bson:"cipher_text" json:"cipher_text"`
+	EncryptedAesKey string             `bson:"encrypted_aes_key" json:"encrypted_aes_key"`
+	OwnerID         string             `bson:"owner_id" json:"owner_id"`
+	ReceiverID      string             `bson:"receiver_id" json:"receiver_id"`
 }
 
 type CreateNoteRequest struct {
