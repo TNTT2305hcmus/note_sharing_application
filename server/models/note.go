6a29030f53b5be58e17533519e31a621f31b4bf5
quocthaiLN <quocthailenguyen41@gmail.com>
2025-12-08 13:48:43 +0700

    hoan thanh api ben server, ma hoa note ben client

--- server/models/note.go
+++ server/models/note.go
@@ -6,11 +6,9 @@ import (
 
 type Note struct {
 	ID              primitive.ObjectID `bson:"_id,omitempty" json:"note_id"` // omitempty = nếu trường rỗng thì tự sinh ID
-	Title           string             `bson:"title" json:"title"`
 	CipherText      string             `bson:"cipher_text" json:"cipher_text"`
 	EncryptedAesKey string             `bson:"encrypted_aes_key" json:"encrypted_aes_key"`
 	OwnerID         string             `bson:"owner_id" json:"owner_id"`
-	ReceiverID      string             `bson:"receiver_id" json:"receiver_id"`
 }
 
 type CreateNoteRequest struct {
